4e47bb689d571ad91f5ad41f11564389a9ed6931
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-06-01 14:56:47 +0530

    adding the basic config

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+import { Nunito } from 'next/font/google'
+
+const font = Nunito({
+  subsets:["latin"],
+})
+
+export const metadata = {
+  title: 'StayFinder | Book your stay',
+  description: 'Hotel booking platform',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
