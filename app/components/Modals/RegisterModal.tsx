6e62d91eccbc33fce8d1af99d12ce4d177d14282
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-06-04 11:03:47 +0530

    added useRegisterModal

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -11,13 +11,51 @@ import {
 } from 'react-hook-form';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import Modals from './Modals';
 
 
 const RegisterModal = () => {
+    const registerModal = useRegisterModal();
+    const[isLoading, setIsLoading] = useState(false);
+
+    const{
+        register,
+        handleSubmit,
+        formState: {
+            errors,
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        axios.post('/api/register',data)
+            .then(()=>{
+                registerModal.onClose();
+            })
+            .catch((error) =>{
+                console.log(error)
+            })
+            .finally(()=>{
+                setIsLoading(false);
+            })
+    }   
+
   return (
-    <div>
-        
-    </div>
+    <Modals
+        disabled={isLoading}
+        isOpen={registerModal.isOpen}
+        title="Register"
+        actionLabel='Continue'
+        onClose={registerModal.onClose}
+        onSubmit={handleSubmit(onSubmit)}
+    />
   )
 }
 
