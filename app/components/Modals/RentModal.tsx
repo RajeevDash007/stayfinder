bd36adc00f34f24f3838a9faab57baebb6afaf11
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-06-14 15:17:49 +0530

    added map structure for location

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -7,6 +7,7 @@ import { categories } from '../Navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
+import Map from '../Map';
 
 enum STEPS {
   CATEGORY = 0,
@@ -120,6 +121,7 @@ const RentModal = () => {
               value={location}            
               onChange={(value) => setCustomValue('location', value)}
             />
+            <Map />
         </div>
       )
     }
