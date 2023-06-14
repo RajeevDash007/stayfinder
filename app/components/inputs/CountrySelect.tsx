f46728876229026743c7189f0abf8d1e4ad39a86
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-06-14 13:01:10 +0530

    added country select option

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,12 +1,35 @@
 'use client';
 
+import useCountries from '@/app/hooks/useCountries';
 import React from 'react';
 import Select from 'react-select';
 
-const CountrySelect = () => {
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[],
+  region: string;
+  value: string;
+}
+
+interface CountrySelectProps{
+  value?: CountrySelectValue;
+  onChange: (value: CountrySelectValue) => void;
+}
+
+const CountrySelect:React.FC<CountrySelectProps> = ({
+  value,
+  onChange
+}) => {
+
+  const {getAll} = useCountries();
   return (
     <div>
-      
+        <Select 
+          placeholder="Location"
+          isClearable
+          options={getAll()}
+        />
     </div>
   )
 }
