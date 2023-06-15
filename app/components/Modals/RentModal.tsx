64842f6bb074390ac73f09b7c7a4a06a697eb143
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-06-15 12:53:38 +0530

    added map in the form

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -7,7 +7,7 @@ import { categories } from '../Navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
-import Map from '../Map';
+import dynamic from 'next/dynamic';
 
 enum STEPS {
   CATEGORY = 0,
@@ -48,6 +48,10 @@ const RentModal = () => {
 
     const category = watch('category');
     const location = watch('location');
+
+    const Map = useMemo(() => dynamic(() => import('../Map'),{
+      ssr:false
+    }),[location]);
 
     const setCustomValue = (id: string, value: any) => {
       setValue(id, value, {
@@ -121,7 +125,9 @@ const RentModal = () => {
               value={location}            
               onChange={(value) => setCustomValue('location', value)}
             />
-            <Map />
+            <Map 
+              center={location?.latlng}
+            />
         </div>
       )
     }
