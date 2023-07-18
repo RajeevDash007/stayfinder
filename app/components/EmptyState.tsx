843f508c0cccf58c6bc89adc8b88e7e0b27e82e6
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-07-18 21:44:04 +0530

    added Heading for the EmptyState and styling and added a button to remove all the filters

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,9 +1,46 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react'
+import Heading from './Heading';
+import Button from './Button';
 
-function EmptyState() {
+interface EmptyState{
+    title?: string;
+    subtitle?:string;
+    showReset?: boolean;
+}
+
+const  EmptyState: React.FC<EmptyState> =  ({
+    title = "No Listings Found",
+    subtitle = "Try to find something else",
+    showReset
+}
+) => {
+    const router = useRouter();
   return (
-    <div>
-      Empty 
+    <div
+        className='
+            h-[60vh]
+            flex
+            flex-col
+            gap
+            justify-center
+            items-center
+        '
+    >
+      <Heading 
+        title={title}
+        subtitle={subtitle}
+      />
+      <div className='w-48 mt-4'>
+            {showReset && (
+                <Button
+                    outline
+                    label='Remove all filters'
+                    onClick={() => router.push('/')}
+                />
+            )}
+      </div>
     </div>
   )
 }
