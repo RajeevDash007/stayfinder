f83d3d27543c6c9a7e3de885b39d36dfb73f4b5e
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-07-22 11:41:25 +0530

    added handleCancel function

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,13 +1,42 @@
+'use client';
+import useCountries from '@/app/hooks/useCountries';
+import { SafeUser } from '@/app/types';
 import { Listing, Reservation } from '@prisma/client'
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useCallback } from 'react'
 
 interface ListingCardProps{
     data: Listing;
     reservation?: Reservation;
-    onAction?: (id: string)
+    onAction?: (id: string) => void;
+    disabled?: boolean;
+    actionLabel?: string;
+    actionId?: string;
+    currentUser?: SafeUser | null;
 }
 
-const ListingCard = () => {
+const ListingCard:React.FC<ListingCardProps> = ({
+    data,
+    reservation,
+    onAction,
+    disabled,
+    actionLabel,
+    actionId = "",
+    currentUser
+}) => {
+    const router = useRouter();
+    const {getByValue} = useCountries();
+    const location = getByValue(data.locationValue);
+    const handleCancel = useCallback(
+        (e: React.MouseEvent<HTMLButtonElement>) => {
+            e.stopPropagation();
+
+            if(disabled){
+                return;
+            }
+
+            onAction?.(actionId);
+        }, [onAction,actionId,disabled])
   return (
     <div>
        Listing Card
