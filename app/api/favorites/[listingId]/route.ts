24bae49b7004adb275d3a3f9298d94caca2fee23
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-07-30 09:07:38 +0530

    added route for favorite cards

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+interface IPrarams{
+    listingId?: string;
+}
+
+export default async function POST(
+    request: Request,
+    { params } : { params: IPrarams }
+){
+    const currentUser = await getCurrentUser();
+    if(!currentUser){
+        return NextResponse.error();
+    }
+
+    const {listingId} = params;
+
+    if(!listingId || typeof listingId != 'string'){
+        throw new Error('Invalid ID');
+    }
+
+    let favoriteIds = [...(currentUser.favoriteIds || [])]
+
+    favoriteIds.push(listingId);
+
+    const user = await prisma.user.update({
+        where:{
+            id: currentUser.id
+        },
+        data:{
+            favoriteIds
+        }
+    });
+
+    return NextResponse.json(user);
+}
